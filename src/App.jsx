5e0561523328e4506cb435b39a08b86c521373e7
AnneLeina <117903163+AnneLeina@users.noreply.github.com>
2026-06-13 16:53:51 +0300

    Fix Login import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css'
 import "bootstrap/dist/css/bootstrap.min.css"
-import Login from "./Components/Login"
+import Login from "./Components/Login.jsx"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Dashboard from "./Components/Dashboard";
 import Home from './Components/Home';
